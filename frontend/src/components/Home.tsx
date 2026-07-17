c217d216d31a9d31d0424fd72e1bdd5b8ff8a83e
ChandanaThoti <https://github.com/ChandanaThoti>
2026-07-17 14:27:31 +0530

    Remove console log and index unwanted styles

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,31 +1,18 @@
-import { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client/react";
 import { GET_STUDENT_COURSE_FACULTY } from "../graphql/queries";
-import { Student } from "../types/universityType";
-type getStudent = {
-    student: Student
-}
+
 const Home = () => {
-    const [student, setStudent] = useState<Student>()
-
-    const { data } = useQuery(GET_STUDENT_COURSE_FACULTY);
-    const getStudent = () => {
-        if (data) {
-            const { student } = data;
-            const { id, name, department, courses } = student!;
-            setStudent({ id, name, department, courses })
-
-        }
-    }
-    useEffect(() => { getStudent() }, [])
+    const { loading, error, data } = useQuery(GET_STUDENT_COURSE_FACULTY);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
 
     return (
         <div>
             <center>
                 <div style={{ backgroundColor: "beige", width: 500, padding: 30 }}>
-                    <h3>Student Name: {student?.name}</h3>
-                    <p>Department: {student?.department}</p>
-                    {student?.courses.map((course) =>
+                    <h3>Student Name: {data.student?.name}</h3>
+                    <p>Department: {data.student?.department}</p>
+                    {data.student?.courses.map((course) =>
                         <div>
                             <h4>Course Name: {course.title}</h4>
                             <p>Credits: {course.credits}</p>
